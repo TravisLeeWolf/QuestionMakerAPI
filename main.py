8191d039fc853aafe70003e59bd5c47105e257da
Travis Naidoo <42667524+TravisLeeWolf@users.noreply.github.com>
2022-07-07 08:14:16 +0900

    Add from template branch

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
+# Mock data
+import mock_data
+
 app = Flask(__name__)
 Bootstrap(app)
+
+data = mock_data.data
+grade = [
+    "Junior High Grade 1",
+    "Junior High Grade 2",
+    "Junior High Grade 3",
+    "Elementary Grade 6",
+    "Elementary Grade 5",
+    "Elementary Grade 4",
+    "Elementary Grade 3",
+    "Elementary Grade 2",
+    "Elementary Grade 1",
+]
 
 """
 Main page where list of questions are viewed
 """
 @app.route("/")
 def main_page():
-    return render_template("index.html")
+    return render_template("index.html", grade=grade, data=data)
 
 
+@app.route("/viewer")
+def view():
+    return render_template("viewer.html")
+
+
+@app.route("/maker")
+def make():
+    return render_template("maker.html")
+
+
+@app.route("/helper")
+def helper():
+    return render_template("api_helper.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ mock_data.py
@@ -0,0 +1,22 @@
+data = [
+    {
+        'pk':1,
+        'id':1,
+        'text':'How is the weather today?',
+        'translation_text':'今日の天気はどうですか',
+        'tags':['easy','weather'],
+        'grades':'es4',
+        'chapter':2,
+        'pic_url':'https://1.bp.blogspot.com/-liFU57C_IsU/Ur1GFHnxsCI/AAAAAAAAcW8/hTvjknYnVdI/s800/job_otenki_oneesan.png'
+    },
+    {
+        'pk':2,
+        'id':2,
+        'text':'What animal do you like?',
+        'translation_text':'何の動物が好きですか',
+        'tags':['easy','animals'],
+        'grads':'es4',
+        'chapter':3,
+        'pic_url':'https://3.bp.blogspot.com/-_ItSdd9RH3A/UnyF8q9-t3I/AAAAAAAAaXk/jpUB-4C6KQY/s800/zoo_doubutsuen.png'
+    }
+    ]
